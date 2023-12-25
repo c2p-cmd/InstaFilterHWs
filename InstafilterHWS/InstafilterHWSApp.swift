9e15e9d4bec73149aa7faf59a36e62459c2ccdc1
Sharan Thakur <sharandt19@live.com>
2023-12-25 16:54:07 +0530

    Initial Commit

---
+++ InstafilterHWS/InstafilterHWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterHWSApp.swift
+//  InstafilterHWS
+//
+//  Created by Sharan Thakur on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterHWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
